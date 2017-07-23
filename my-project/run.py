0d38bd92b61991667855b393ccf4f2375e8da4ed
Layla <layla.rowen@gmail.com>
2017-07-23 02:14:03 +0000

    added instance folder and deleted templates and views for about and home page

--- my-project/run.py
+++ my-project/run.py
@@ -1,9 +1,12 @@
 # run.py
 
-from app import app
+from app import create_app
 import os
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
-    
+
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
